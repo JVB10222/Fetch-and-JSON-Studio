7c5e13e8ee270bfa366b15493e3b111ab702941c
JVB10222 <JVB10222@outlook.com>
2022-08-11 20:58:23 -0500

    added the bonus missions to complete this assignment

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-window.addEventListener("load", function(){
-    const weatherEndPoint = "https://handlers.education.launchcode.org/static/astronauts.json";
 
-    const fetchPromise = fetch(weatherEndPoint);
+function init(){
+        const astronautsApi = "https://handlers.education.launchcode.org/static/astronauts.json";
+        const container =  document.getElementById("container");
+        fetch(astronautsApi).then(function(response){
+            response.json().then(function(astronauts){
+                
+                for(const astronaut of astronauts.sort((a,b) => b.hoursInSpace - a.hoursInSpace)){
+                    container.innerHTML = `${container.innerHTML}
+                    <div class="astronaut">
+                        <div class="bio">
+                            <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                            <ul>
+                                <li>Active: <span class="${astronaut.active ? "active" : "inactive"}">${astronaut.active}</span></li>
+                                <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                                <li>Skills: ${astronaut.skills}</li>
+                            </ul>
+                        </div>
+                        <img class="avatar" src=${astronaut.picture}>
+                    </div>`;
+                }
+                const count = document.getElementById("astroCount");
+                count.innerHTML = `Total Count: ${astronauts.length}`;
 
-    let temp;
-
-    fetchPromise.then(function(response){
-        response.json().then(function(data){
-            console.log(data);
+            });
         });
-    });
-});
+    }
+
+window.addEventListener("load", init);
 
