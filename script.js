6cc696638517743f1b5f9c93d982338e1810efdb
JVB10222 <JVB10222@outlook.com>
2022-08-11 19:39:51 -0500

    test

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// TODO: add code here
+window.addEventListener("load", function(){
+    const weatherEndPoint = "https://handlers.education.launchcode.org/static/astronauts.json";
+
+    const fetchPromise = fetch(weatherEndPoint);
+
+    let temp;
+
+    fetchPromise.then(function(response){
+        response.json().then(function(data){
+            console.log(data);
+        });
+    });
+});
+
